b729cc2c9a9f702a37dcc6a3e2d53dbeb393f7a9
deepti <2021399693.deepti@ug.sharda.ac.in>
2023-07-11 18:17:17 +0530

    All the Api's of add-to-cart are done

--- project/addToCart/controller.js
+++ project/addToCart/controller.js
@@ -8,42 +8,43 @@ class AddToCartController {
   // Api to add the items to user's cart
 
   async AddToCartAdd(req, res) {
-    const { userId, itemId ,quantity} = req.body;
+    const { userId, itemId, quantity } = req.body;
 
     // check if user exits or not
     const isUserExist = await AddToCartModel.findOne({ userId });
+    console.log("user exists", isUserExist);
 
     // if exists then add the data
     if (isUserExist) {
       //   use $addToSet ,to prevent adding of duplicate items.and use findoneandupdate means that the useer already exists and we are adding item to its existing item cart
       const data = await AddToCartModel.findOneAndUpdate(
         { userId },
-        { $addToSet: { itemId: itemId } },
-        { quantity },
-      )
-
+        { $addToSet: { products: { itemId, quantity } } }
+      );
+      console.log("data", data);
       res.send({ message: "success", data: data });
     } else {
       //if user doesn't exists then create the cart for new user.
 
       const AddNewItems = new AddToCartModel({
-        ...req.body,
+        userId,
+        products: [{ itemId, quantity }],
       });
-      await AddNewItems.save();
-      res.send({ success: true });
+      const saves = await AddNewItems.save();
+      res.send({ success: true, data: saves });
     }
   }
 
   // api to update the quantity and the price of the itemm.
   async AddToCartItemUpdate(req, res) {
-    const { quantity, price, user } = req.body;
+    const { quantity, price, userId } = req.body;
     let OB = {};
     if (quantity) OB.quantity = quantity;
     if (price) OB.price = price;
 
     try {
       // Find the user's cart and update the item quantity
-      const updatedCart = await AddToCartModel.findOneAndUpdate({ user }, OB);
+      const updatedCart = await AddToCartModel.findOneAndUpdate({ userId }, OB);
 
       if (!updatedCart) {
         return res.status(404).json({ message: "Cart or item not found" });
@@ -62,21 +63,24 @@ class AddToCartController {
   // Api to get the details of the cart items.
   async AddToCartItemList(req, res) {
     try {
-      const find = await AddToCartModel.find().populate();
+      const find = await AddToCartModel.find().populate("products.itemId");
       res.send({ success: true, data: find });
+      console.log("console");
+
     } catch (error) {
       console.log("error", error);
       res.send(error);
+      console.log("CONSOLE");
     }
   }
 
   // Api to delete the specific item from the cart.
   async AddToCartItemDelete(req, res) {
     try {
-      const { userId, productId } = req.body;
+      const { userId, itemId } = req.body;
       const update = await AddToCartModel.updateOne(
         { userId },
-        { $pull: { cartItems: productId } }
+        { $pull: { products: itemId } }
       );
       res.send({ success: true, data: update });
       console.log("Kuch to hua");
